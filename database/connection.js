8341a65a3ba55101b9ebfece33cda14a0cd83fdc
Bojtronic <bojtronic@gmail.comc>
2026-03-16 18:53:45 -0600

    BD local

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  //user: 'postgres',
-  user: 'ss',   
-  //host: 'localhost',
-  host: 'dpg-d6jijo6a2pns73f6gkvg-a',   
-  //database: 'ae_bd',
+  user: 'postgres',
+  //user: 'ss',   
+  host: 'localhost',
+  //host: 'dpg-d6jijo6a2pns73f6gkvg-a',   
   database: 'ae_bd',
-  //password: '12345',
-  password: 'byIOdcqpWJViWBZEs70PkNALaTO0hGIi',
-  //port: 5432
+  //database: 'ae_bd',
+  password: '12345',
+  //password: 'byIOdcqpWJViWBZEs70PkNALaTO0hGIi',
   port: 5432
+  //port: 5432
 });
 
 module.exports = pool;
