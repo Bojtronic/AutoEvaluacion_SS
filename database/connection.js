30ae4a67934463893801547135ea8abfae5f3def
Bojtronic <bojtronic@gmail.comc>
2026-03-21 12:32:05 -0600

    Nueva base de datos actualizada

--- database/connection.js
+++ database/connection.js
@@ -4,11 +4,11 @@ const pool = new Pool({
   //user: 'postgres',
   user: 'ss',   
   //host: 'localhost',
-  host: 'dpg-d6jijo6a2pns73f6gkvg-a',   
+  host: 'dpg-d6vdtpa4d50c73funep0-a',   
   //database: 'ae_bd',
-  database: 'ae_bd',
+  database: 'ae_bd_2bjz',
   //password: '12345',
-  password: 'byIOdcqpWJViWBZEs70PkNALaTO0hGIi',
+  password: 'R9J51LF5alwrlwwjPN5oD03grD1VXhB8',
   //port: 5432
   port: 5432
 });
